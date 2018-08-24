b3156be04f01b9093ca86fc10fc071c869a86bd2
LEO <leo_26198@hotmail.com>
2018-08-24 07:18:35 -0300

    alytertrf

--- WebApplication1/WebApplication1/Models/CadastroImovel/ValoresFolder/ValorVenda.cs
+++ WebApplication1/WebApplication1/Models/CadastroImovel/ValoresFolder/ValorVenda.cs
@@ -8,13 +8,13 @@ namespace WebApplication1.Models.CadastroImovel.Valores
     public class ValorVenda:Valor
     {
         public double ValorDeVenda { get; set; }
-        public bool Permuta { get; set; }
+        //public bool Permuta { get; set; }
         public bool PodeFinanciar { get; set; }
         public bool EstaEsscriturado { get; set; }
         public bool EstaAverbado { get; set; }
-        public bool EstaFinanciado { get; set; }
-        public double ValorDivida { get; set; }
-        public int NumeroParcelasRestantes { get; set; }
-        
+        //public bool EstaFinanciado { get; set; }
+        //public double ValorDivida { get; set; }
+        //public int NumeroParcelasRestantes { get; set; }
+     //forma  pagamento:   
     }
 }
